1dee2f3fde7b140b6222a787190b596ec4035708
Byiringiro Aloys <byiringiroaloys8@gmail.com>
2025-08-09 14:24:36 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import apis from './api/axiosConfig.js';
+import Layout from "./components/Layout.jsx";
+import {Routes,Route} from 'react-router-dom';
+import Home from './components/home/Home.jsx';
+
+export default function App() {
+
+  const [movies, setMovies] = useState({});
+
+  const getMovies = async () => {
+    try {
+
+      const response = await apis.get("/api/v1/movies");
+      setMovies(response.data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }
+useEffect(()=>{
+  getMovies();
+},[])
+
+
+  return (
+    <div>
+        <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route path="/" element={<Home movies={movies}/>}></Route>
+            </Route>
+        </Routes>
+    </div>
+  )
+}
+
